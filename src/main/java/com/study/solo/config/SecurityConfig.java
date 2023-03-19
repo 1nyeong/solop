0d3232e5a352bb0d60a6efe7f8f7f5b0697436b0
1nyeong <inyeong0726@gmail.com>
2023-03-20 02:08:16 +0900

    로그인 페이지, 컨트롤러

--- src/main/java/com/study/solo/config/SecurityConfig.java
+++ src/main/java/com/study/solo/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.study.solo.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,5 +18,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.httpBasic().disable();
+        http.authorizeHttpRequests()
+                .antMatchers("/mypage/**", "/security/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/account/login")
+                .loginProcessingUrl("/account/login")
+                .defaultSuccessUrl("index");
     }
 }
---
+++ src/main/java/com/study/solo/web/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.study.solo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/index"})
+    public String index(){
+        return "index";
+    }
+}
